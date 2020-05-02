562f7f8fdaf390fa86db4b72a5a3050ca1d22a6c
Kutub <kutubuddinmondal512@gmail.com>
2020-05-02 10:39:21 +0530

    Now Techs are comming from our App label state by Redux

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import M from "materialize-css/dist/js/materialize.min.js";
 import { updateLog } from "../../actions/logAction";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
+import M from "materialize-css/dist/js/materialize.min.js";
 const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
@@ -63,9 +64,7 @@ const EditLogModal = ({ current, updateLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
